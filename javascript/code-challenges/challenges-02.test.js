69eccb2889216a78f288641c583a34d253a9e449
AhmadSailik <asailk1993@gmail.com>
2021-12-06 18:04:37 +0200

    solve-7-map-challenge

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -100,6 +100,8 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
+  let newArr=arr.map(item=>item.charCodeAt(0));
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -114,6 +116,15 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 
 const evenOdd = (arr) => {
   // Solution code here...
+  let newArr=arr.map(item=>{
+    if(typeof item=="string"){
+      return "N/A"
+    }else if(item%2==0){
+      return "even"
+    }else if(item%2!=0){
+      return "odd"
+    }
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -271,14 +282,14 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
