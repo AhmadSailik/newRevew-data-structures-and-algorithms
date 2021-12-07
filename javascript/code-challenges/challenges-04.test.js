f5e77c876c3ed94da279c0713ff08ef4904dcc40
AhmadSailik <asailk1993@gmail.com>
2021-12-08 00:25:41 +0200

    solve-13-sort-challenge

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -330,6 +330,7 @@ You DO NOT need to use your solution to Challenge 9 in completing Challenge 10.
 
 const sortSchedule = (arr) => {
   // Solution code here...
+  return sortMeetingsByDay(arr);
 };
 
 /* ------------------------------------------------------------------------------------------------
